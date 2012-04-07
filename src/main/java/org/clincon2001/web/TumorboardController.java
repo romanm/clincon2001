d8f27a9837c145ccf1506993e50132fb05547914
Mishchenko <roman.mishchenko@gmail.com>
2012-04-07 23:58:31 +0200

    work commit

--- src/main/java/org/clincon2001/web/TumorboardController.java
+++ src/main/java/org/clincon2001/web/TumorboardController.java
@@ -2,8 +2,10 @@ package org.clincon2001.web;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.clincon2001.domain.Clincon;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/tumorboard")
@@ -15,5 +17,14 @@ public class TumorboardController {
 	public String home(Model uiModel) {
 		logger.info("-------BEGIN---------------");
 		return "tumorboard/home";
+	}
+	@RequestMapping(value = "/{id}", produces = "text/html")
+	public String show(@PathVariable("id") Long id, Model uiModel) {
+		logger.info("-------BEGIN---------------"+id);
+		Clincon findClincon = Clincon.findClincon(id);
+		uiModel.addAttribute("clincon", findClincon);
+		uiModel.addAttribute("itemId", id);
+		logger.info("END");
+		return "tumorboard/show";
 	}
 }
