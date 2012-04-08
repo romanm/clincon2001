507d9d8f44c0dec2d9ed127f29b9e9f9494842b4
Mishchenko <roman.mishchenko@gmail.com>
2012-04-08 16:27:33 +0200

    first tagx

--- src/main/java/org/clincon2001/web/TumorboardController.java
+++ src/main/java/org/clincon2001/web/TumorboardController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/tumorboard")
 @Controller
@@ -27,4 +28,19 @@ public class TumorboardController {
 		logger.info("END");
 		return "tumorboard/show";
 	}
+
+	@RequestMapping(value = "/list", produces = "text/html")
+	public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
+		if (page != null || size != null) {
+			int sizeNo = size == null ? 10 : size.intValue();
+			final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+			uiModel.addAttribute("clincons", Clincon.findClinconEntries(firstResult, sizeNo));
+			float nrOfPages = (float) Clincon.countClincons() / sizeNo;
+			uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+		} else {
+			uiModel.addAttribute("clincons", Clincon.findAllClincons());
+		}
+		return "tumorboard/list";
+	}
+
 }
